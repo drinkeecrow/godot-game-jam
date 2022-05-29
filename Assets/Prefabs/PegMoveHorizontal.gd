18d290addae7bef7576c39ef5010b0ec2a822c81
gromit103 <gromit103@gmail.com>
2022-05-28 22:09:20 -0500

    Lots of stuff
    Added peg logic, level logic, backgrounds and more

---
+++ Assets/Prefabs/PegMoveHorizontal.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+var hMove = 1000
+var vMove = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
