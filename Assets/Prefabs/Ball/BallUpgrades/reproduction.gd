4ce66dd7c36309c4686b152987b812a003bf2bf1
Curtis Green <AllynGreen@gmail.com>
2022-06-03 15:48:49 -0400

    Cleaned up some files, and directory tree a little

--- Assets/Prefabs/Ball/BallUpgrades/reproduction.gd
+++ Assets/Prefabs/Ball/BallUpgrades/reproduction.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-var ballRes = load("res://Assets/Prefabs/Ballmeba.tscn")
+var ballRes = load("res://Assets/Prefabs/Ball/Ballmeba.tscn")
 onready var ballNode = self.owner
 var hits = 0
 var enabled = true
